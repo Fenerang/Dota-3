0a9c9b257b2b266de5e53f5b2fab017b0fa7569a
Fenerang <92585804+Fenerang@users.noreply.github.com>
2021-11-09 17:32:38 +0400

    Dota3  v1.2
    *Add HP bar + HP system

--- script.js
+++ script.js
@@ -21,8 +21,26 @@ let arrPicEnemy=[];
     arrPicEnemy['left']=picEnemyLeft;
     arrPicEnemy['right']=picEnemyRight;
 
+let picLife = new Image();
+    picLife.src = "./pic/Heart.png"
+
+let picRuna = new Image();
+picRuna.src = "./pic/Runa.png"
+
+
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
+
+function generateRandomPosition(pic,xMin,xMax,yMin,yMax){
+    let x = Math.random()*((xMax-pic.width)-xMin)+xMin,
+    y = Math.random()*((yMax-pic.height)-yMin)+yMin;
+    return [x, y];
+}
+
+let positionRuna=[0,0];
+function newPositionRuna(){
+    positionRuna=generateRandomPosition(picRuna,0,innerWidth,window.innerHeight*0.7,innerHeight*0.193)
+}
 
 function resizeImg(img, percent){
     let prop;
@@ -38,21 +56,54 @@ function resizeImg(img, percent){
     
 }
 let startGame = false;
-let xPlayer = 50, yPlayer = 600, speedPlayer= 5, navPlayer = 'right',
-    xEnemy = 800, yEnemy = 650,speedEnemy = 5, navEnemy = 'left'
+let xPlayer = 50, yPlayer = 600, speedPlayer= 10, navPlayer = 'right',
+    xEnemy = 800, yEnemy = 650,speedEnemy = 10, navEnemy = 'left',boardPicPlayer=0,boardPicEnemy=0;
+    countLife=5;
 
+function boardPic(pic, x, y){
+    let picRight, picBottom;
+    picRight = x + pic.width;
+    picBottom = y + pic.height;
+    return [picRight, picBottom];        
+    }
 function draw(){
     let picPlayer = arrPicPlayer[navPlayer]
     let picEnemy = arrPicEnemy[navEnemy]
     resizeImg(picPlayer,8)
     resizeImg(picEnemy,8)
+    resizeImg(picLife,5)
+    resizeImg(picRuna,5)
+     boardPicPlayer = boardPic(picPlayer, xPlayer, yPlayer);
+   
+     boardPicEnemy = boardPic(picEnemy, xEnemy, yEnemy);
+
 
     function startPosition(){
         xPlayer = window.innerWidth*0.05;
         yPlayer = window.innerHeight*0.7;
+        xEnemy = window.innerWidth*0.65;
+        yEnemy = window.innerHeight*0.7;
     }
 
     function moveEnemy(){
+        function collisionEnemy(){
+            if(boardPicPlayer[0]>xEnemy && xPlayer<boardPicEnemy[0] && yPlayer<boardPicEnemy[1] && yEnemy<boardPicPlayer[1]){
+                countLife--;
+                if (countLife<=0){
+                    let NewGame=confirm('GG WP:(\n Начать заново?)')
+                    if (NewGame){
+                        countLife=5;
+                    }
+                    else{
+                        clearTimeout(timerMoveEnemy);
+                        startPosition();
+                        return;
+                    }
+                }
+                startPosition();
+            }
+        }
+        collisionEnemy();
         let timerMoveEnemy = setTimeout(()=>{
             if(xEnemy>(xPlayer+picPlayer.width)){
                 xEnemy --;
@@ -66,13 +117,20 @@ function draw(){
         },speedEnemy);
     }
     if(startGame){
+        startPosition();
+        newPositionRuna();
         moveEnemy();
         startGame=false;
     }
     ctx.drawImage(picBackground, 0, 0, window.innerWidth, window.innerHeight)
     ctx.drawImage(picPlayer, xPlayer, yPlayer, picPlayer.width, picPlayer.height)
     ctx.drawImage(picEnemy, xEnemy, yEnemy, picEnemy.width, picEnemy.height)
-
+    for(let i=0; i<countLife;i++){
+        let yLife=innerHeight*0.1
+        let xLife=innerWidth*0.05+picLife.width*i;
+    ctx.drawImage(picLife, xLife, yLife, picLife.width, picLife.height)
+    ctx.drawImage(picRuna, positionRuna[0], positionRuna[1], picRuna.width, picRuna.height)
+    }
 }
 
 picBackground.onload = draw;
